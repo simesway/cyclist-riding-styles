c73b6c8bd6e045da9ae36483348e567538fdd3c1
Simon Eiber <eiber.simon@web.de>
2025-12-10 12:34:57 +0100

    add functions to calculate magnitude, lateral and longitudinal accelerations

--- src/features/vehicle_dynamics.py
+++ src/features/vehicle_dynamics.py
@@ -21,3 +21,26 @@ def lateral_velocity(df: pd.DataFrame) -> pd.Series:
   cos_yaw = np.cos(df['rotation_z'])
   sin_yaw = np.sin(df['rotation_z'])
   return -df['velocity_x'] * sin_yaw + df['velocity_y'] * cos_yaw
+
+
+def acceleration(df: pd.DataFrame) -> pd.Series:
+  """Compute total acceleration magnitude."""
+  return np.hypot(df['acceleration_x'], df['acceleration_y'])
+
+
+def longitudinal_acceleration(df: pd.DataFrame) -> pd.Series:
+  """
+  Compute longitudinal acceleration along the vehicle's forward direction.
+  """
+  cos_yaw = np.cos(df['rotation_z'])
+  sin_yaw = np.sin(df['rotation_z'])
+  return df['acceleration_x'] * cos_yaw + df['acceleration_y'] * sin_yaw
+
+
+def lateral_acceleration(df: pd.DataFrame) -> pd.Series:
+  """
+  Compute lateral acceleration perpendicular to the vehicle's forward direction.
+  """
+  cos_yaw = np.cos(df['rotation_z'])
+  sin_yaw = np.sin(df['rotation_z'])
+  return -df['acceleration_x'] * sin_yaw + df['acceleration_y'] * cos_yaw
