530a8b352c73e2338c88900109822ec184c70028
Simon Eiber <eiber.simon@web.de>
2026-02-05 19:05:55 +0100

    added subsampling for metric calculations

--- src/clustering/grid_search.py
+++ src/clustering/grid_search.py
@@ -1,7 +1,9 @@
-from itertools import product
-from tqdm import tqdm
-import pandas as pd
 import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from itertools import product
+from sklearn.utils import resample
 
 from clustering.clusterer import KMeansClusterer
 from clustering.feature_selection import FeatureSelector
@@ -24,6 +26,7 @@ def grid_search(
   getter,
   param_grid,
   min_cluster_frac=0.05,
+  subsample_frac=1.0,
   n_runs=5,
 ):
   results = []
@@ -56,7 +59,14 @@ def grid_search(
       if len(unique) < k or counts.min() < min_cluster_frac * len(data):
         continue
 
-      stability = pipeline.stability_test(data, n_runs=n_runs, pbar=False)
+      subsample = resample(
+        data,
+        replace=False,
+        n_samples=int(subsample_frac * len(data)),
+        stratify=labels
+      )
+
+      stability = pipeline.stability_test(subsample, n_runs=n_runs, pbar=False)
       ex_var = pipeline.pca.explained_variance()
 
       results.append({
@@ -78,7 +88,8 @@ def grid_search(
         **{f"cluster_{i}": c for i, c in enumerate(counts)},
       })
 
-    except Exception:
+    except Exception as e:
+      print(e)
       continue
 
   return pd.DataFrame(results)
