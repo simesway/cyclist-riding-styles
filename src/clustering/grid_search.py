6bc4144658c8c67e7155dceaf805883f190bf8c8
Simon Eiber <eiber.simon@web.de>
2026-02-09 15:16:18 +0100

    refactor grid_search to enhance stability testing and metrics calculation

--- src/clustering/grid_search.py
+++ src/clustering/grid_search.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from tqdm import tqdm
 from itertools import product
-from sklearn.utils import resample
 
 from clustering.clusterer import KMeansClusterer
 from clustering.feature_selection import FeatureSelector
@@ -25,9 +24,10 @@ def grid_search(
   adapter_factory,
   getter,
   param_grid,
-  min_cluster_frac=0.05,
   subsample_frac=1.0,
+  silhouette_subsample=0.8,
   n_runs=5,
+  seed=0
 ):
   results = []
 
@@ -53,39 +53,36 @@ def grid_search(
         clusterer=KMeansClusterer(k=k),
       )
 
-      X, labels, valid = pipeline.run(data)
-
-      unique, counts = np.unique(labels, return_counts=True)
-      if len(unique) < k or counts.min() < min_cluster_frac * len(data):
-        continue
-
-      subsample = resample(
+      metrics, silhouette = pipeline.stability_test(
         data,
-        replace=False,
-        n_samples=int(subsample_frac * len(data)),
-        stratify=labels
+        subsample_frac=subsample_frac,
+        silhouette_subsample=silhouette_subsample,
+        n_runs=n_runs,
+        pbar=False, seed=seed
       )
-
-      stability = pipeline.stability_test(subsample, n_runs=n_runs, pbar=False)
       ex_var = pipeline.pca.explained_variance()
+
+      ari_subsample_median = metrics.loc[metrics.metric == "ari_subsample", "median"].iloc[0]
+      ari_noise_median = metrics.loc[metrics.metric == "ari_noise", "median"].iloc[0]
+      ari_seed_median = metrics.loc[metrics.metric == "ari_seed", "median"].iloc[0]
 
       results.append({
         "corr_threshold": corr,
         "vif_threshold": vif,
         "k": k,
         "components": comp,
-        "silhouette": stability.loc[
-          stability.metric == "silhouette", "median"
+        "silhouette": silhouette,
+        "ari_subsample": ari_subsample_median,
+        "ari_noise": ari_noise_median,
+        "ari_seed": ari_seed_median,
+        "min_cluster_frac_median": metrics.loc[
+          metrics.metric == "min_cluster_frac", "median"
         ].iloc[0],
-        "ari_subsample": stability.loc[
-          stability.metric == "ari_subsample", "median"
-        ].iloc[0],
-        "ari_noise": stability.loc[
-          stability.metric == "ari_noise", "median"
+        "min_cluster_frac_std": metrics.loc[
+          metrics.metric == "min_cluster_frac", "std"
         ].iloc[0],
         "explained_variance": sum(ex_var),
-        "num_components": len(ex_var),
-        **{f"cluster_{i}": c for i, c in enumerate(counts)},
+        "num_components": len(ex_var)
       })
 
     except Exception as e:
